31c659f4f2b9547cd31bf5e1027c0d0a78f72ab9
Evando Junior <evando.junior@live.com>
2018-01-15 17:36:18 -0200

    Enviando o arquivos

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from '../styles/logo.svg';
+import '../styles/App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="App">
+        <footer className="App-footer">
+          <h3 >Developed by Evando Junior </h3>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Footer;
